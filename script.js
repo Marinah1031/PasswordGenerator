5049b23568573407d7a59fda57bad8a2ddb1b8ce
Marinah Tamura <marinahtam@gmail.com>
2023-04-19 15:41:42 -0700

    editing prompts that come up

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+var generateBtn = window.prompt("How many characters would you like the password to contain?")
+if (generateBtn >= 8) {
+  window.confirm("Click OK to confirm including special characters.");
+} else {
+  window.prompt("The number must be atleast 8.");
+};
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
